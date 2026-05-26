808f525a38c07b2f35f69f95b82a852bf6f27220
GargantuaZ <gargantua.z@qq.com>
2026-05-26 15:46:46 +0800

    同步其他BirdSet脚本

---
+++ BirdSet_scripts/descriptions.py
@@ -0,0 +1,184 @@
+_HAWAIIAN_ISLANDS_DESCRIPTION = """This collection contains 635 soundscape recordings with a total duration of almost 
+51 hours, which have been annotated by expert ornithologists who provided 59,583 bounding box labels for 27 different 
+bird species from the Hawaiian Islands, including 6 threatened or endangered native birds. The data were recorded 
+between 2016 and 2022 at four sites across Hawai‘i Island. This collection has partially been featured as test data 
+in the 2022 BirdCLEF competition and can primarily be used for training and evaluation of machine learning 
+algorithms."""
+
+_HAWAIIAN_ISLANDS_CITATION = """@dataset{amanda_navine_2022_7078499,
+  author       = {Amanda Navine and
+                  Stefan Kahl and
+                  Ann Tanimoto-Johnson and
+                  Holger Klinck and
+                  Patrick Hart},
+  title        = {{A collection of fully-annotated soundscape 
+                   recordings from the Island of Hawai'i}},
+  month        = sep,
+  year         = 2022,
+  publisher    = {Zenodo},
+  version      = 1,
+}"""
+
+_SAPSUCKER_WOODS_DESCRIPTION = """This collection contains 285 hour-long soundscape recordings, which have been 
+annotated by expert ornithologists who provided 50,760 bounding box labels for 81 different bird species from the 
+Northeastern USA. The data were recorded in 2017 in the Sapsucker Woods bird sanctuary in Ithaca, NY, 
+USA. This collection has (partially) been featured as test data in the 2019, 2020 and 2021 BirdCLEF competition and 
+can primarily be used for training and evaluation of machine learning algorithms."""
+
+_SAPSUCKER_WOODS_CITATION = """@dataset{stefan_kahl_2022_7079380,
+  author       = {Stefan Kahl and
+                  Russell Charif and
+                  Holger Klinck},
+  title        = {{A collection of fully-annotated soundscape 
+                   recordings from the Northeastern United States}},
+  month        = sep,
+  year         = 2022,
+  publisher    = {Zenodo},
+  version      = 2,
+}"""
+
+_AMAZON_BASIN_DESCRIPTION = """This collection contains 21 hour-long soundscape recordings, which have been annotated 
+with 14,798 bounding box labels for 132 different bird species from the Southwestern Amazon Basin. The data were 
+recorded in 2019 in the Inkaterra Reserva Amazonica, Madre de Dios, Peru. This collection has partially been featured 
+as test data in the 2020 BirdCLEF competition and can primarily be used for training and evaluation of machine 
+learning algorithms."""
+
+_AMAZON_BASIN_CITATION = """@dataset{w_alexander_hopping_2022_7079124,
+  author       = {W. Alexander Hopping and
+                  Stefan Kahl and
+                  Holger Klinck},
+  title        = {{A collection of fully-annotated soundscape 
+                   recordings from the Southwestern Amazon Basin}},
+  month        = sep,
+  year         = 2022,
+  publisher    = {Zenodo},
+  version      = 1,
+}"""
+
+_SIERRA_NEVADA_DESCRIPTION = """This collection contains 33 hour-long soundscape recordings, which have been 
+annotated with 20,147 bounding box labels for 56 different bird species from the Western United States. The data were 
+recorded in 2018 in the Sierra Nevada, California, USA. This collection has partially been featured as test data in 
+the 2021 BirdCLEF competition and can primarily be used for training and evaluation of machine learning algorithms."""
+
+_SIERRA_NEVADA_CITATION = """@dataset{stefan_kahl_2022_7050014,
+  author       = {Stefan Kahl and
+                  Connor M. Wood and
+                  Philip Chaon and
+                  M. Zachariah Peery and
+                  Holger Klinck},
+  title        = {{A collection of fully-annotated soundscape 
+                   recordings from the Western United States}},
+  month        = sep,
+  year         = 2022,
+  publisher    = {Zenodo},
+  version      = 1,
+}"""
+
+_POWDERMILL_NATURE_DESCRIPTION = """Acoustic recordings of soundscapes are an important category of audio data which 
+can be useful for answering a variety of questions, and an entire discipline within ecology, dubbed "soundscape 
+ecology," has risen to study them. Bird sound is often the focus of studies of soundscapes due to the ubiquitousness 
+of birds in most terrestrial environments and their high vocal activity. Autonomous acoustic recorders have increased 
+the quantity and availability of recordings of natural soundscapes while mitigating the impact of human observers on 
+community behavior. However, such recordings are of little use without analysis of the sounds they contain. Manual 
+analysis currently stands as the best means of processing this form of data for use in certain applications within 
+soundscape ecology, but it is a laborious task, sometimes requiring many hours of human review to process 
+comparatively few hours of recording. For this reason, few annotated datasets of soundscape recordings are publicly 
+available. Further still, there are no publicly available strongly-labeled soundscape recordings of bird sounds which 
+contain information on timing, frequency, and species. Therefore, we present the first dataset of strongly-labeled 
+bird sound soundscape recordings under free use license. These data were collected in the Northeastern United States 
+at Powdermill Nature Reserve, Rector, PA. Recordings encompass 385 minutes of dawn chorus recordings collected by 
+autonomous acoustic recorders between the months of April through July 2018. Recordings were collected in continuous 
+bouts on four days during the study period, and contain 48 species and 16,052 annotations. Applications of this 
+dataset may be numerous, and include the training, validation, and testing of certain advanced machine learning 
+models which detect or classify bird sounds."""
+
+_POWDERMILL_NATURE_CITATION = """@dataset{chronister_2021_4656848,
+  author       = {Chronister, Lauren M. and
+                  Rhinehart, Tessa A. and
+                  Place, Aidan and
+                  Kitzes, Justin},
+  title        = {{An annotated set of audio recordings of Eastern 
+                   North American birds containing frequency, time,
+                   and species information}},
+  month        = apr,
+  year         = 2021,
+  publisher    = {Zenodo},
+}"""
+
+_HIGH_SIERRAS_DESCRIPTION = """This collection contains 100 soundscape recordings of 10 minutes duration, which have 
+been annotated with 10,296 bounding box labels for 21 different bird species from the Western United States. The data 
+were recorded in 2015 in the southern end of the Sierra Nevada mountain range in California, USA. This collection has 
+been featured as test data in the 2020 BirdCLEF and Kaggle Birdcall Identification competition and can primarily be 
+used for training and evaluation of machine learning algorithms."""
+
+_HIGH_SIERRAS_CITATION = """@dataset{mary_clapp_2023_7525805,
+  author       = {Mary Clapp and
+                  Stefan Kahl and
+                  Erik Meyer and
+                  Megan McKenna and
+                  Holger Klinck and
+                  Gail Patricelli},
+  title        = {{A collection of fully-annotated soundscape 
+                   recordings from the southern Sierra Nevada
+                   mountain range}},
+  month        = jan,
+  year         = 2023,
+  publisher    = {Zenodo},
+  version      = 1,
+}"""
+
+_COLUMBIA_COSTA_RICA_DESCRIPTION = """This collection contains 34 hour-long soundscape recordings, which have been 
+annotated by expert ornithologists who provided 6,952 bounding box labels for 89 different bird species from Colombia 
+and Costa Rica. The data were recorded in 2019 at two highly diverse neotropical coffee farm landscapes from the 
+towns of Jardín, Colombia and San Ramon, Costa Rica. This collection has partially been featured as test data in the 
+2021 BirdCLEF competition and can primarily be used for training and evaluation of machine learning algorithms."""
+
+_COLUMBIA_COSTA_RICA_CITATION = """@dataset{alvaro_vega_hidalgo_2023_7525349,
+  author       = {Álvaro Vega-Hidalgo and
+                  Stefan Kahl and
+                  Laurel B. Symes and
+                  Viviana Ruiz-Gutiérrez and
+                  Ingrid Molina-Mora and
+                  Fernando Cediel and
+                  Luis Sandoval and
+                  Holger Klinck},
+  title        = {{A collection of fully-annotated soundscape 
+                   recordings from neotropical coffee farms in
+                   Colombia and Costa Rica}},
+  month        = jan,
+  year         = 2023,
+  publisher    = {Zenodo},
+  version      = 1,
+}"""
+
+_NIPS4BPLUS_DESCRIPTION = """The zip file contains 674 individual recording temporal annotations for the training set 
+of the NIPS4B 2013 dataset in the birdsong classifications task (original size of dataset is 687 recordings)."""
+
+_NIPS4BPLUS_CITATION = """@article{Morfi2019,
+author = "Veronica Morfi and Dan Stowell and Hanna Pamula",
+title = "{NIPS4Bplus: Transcriptions of NIPS4B 2013 Bird Challenge Training Dataset}",
+year = "2019",
+month = "7",
+url = "https://figshare.com/articles/dataset/Transcriptions_of_NIPS4B_2013_Bird_Challenge_Training_Dataset/6798548",
+doi = "10.6084/m9.figshare.6798548.v7"
+}"""
+
+_BIRD_DB_DESCRIPTION = """Projects on the acoustic monitoring of animals in natural habitats generally face the 
+problem of managing extensive amounts of data, both needed for – and produced by – observation or experimentation. 
+While there are many publicly accessible databases for recordings themselves, we are aware of none for annotated song 
+sequences. In this paper, we describe our database system of bird vocalizations and introduce our online sample 
+repository for the community of researchers studying the syntax of bird song."""
+
+_BIRD_DB_CITATION = """@article{ARRIAGA201521,
+title = {Bird-DB: A database for annotated bird song sequences},
+journal = {Ecological Informatics},
+volume = {27},
+pages = {21-25},
+year = {2015},
+issn = {1574-9541},
+doi = {https://doi.org/10.1016/j.ecoinf.2015.01.007},
+url = {https://www.sciencedirect.com/science/article/pii/S1574954115000151},
+author = {Julio G. Arriaga and Martin L. Cody and Edgar E. Vallejo and Charles E. Taylor},
+keywords = {Bioacoustics, Bird song, Phrase sequence, Birdsong syntax, Database},
+abstract = {Projects on the acoustic monitoring of animals in natural habitats generally face the problem of managing extensive amounts of data, both needed for – and produced by – observation or experimentation. While there are many publicly accessible databases for recordings themselves, we are aware of none for annotated song sequences. In this paper, we describe our database system of bird vocalizations and introduce our online sample repository for the community of researchers studying the syntax of bird song.}
+}"""
